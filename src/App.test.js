d0222bf0efbcd4ea969cded8296c87dc54f29df8
Freeda Dsouza <fernandes.aaron1@gmail.com>
2022-11-01 16:19:45 +0530

    test file changes

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from "./App";
 
 describe('Test App.js', () => {
 
-    it("Should render app without crashing", () => {
+    it("Should render app without crashing always", () => {
         expect(
             JSON.stringify(
               Object.assign({}, App, { _reactInternalInstance: 'censored' }),
